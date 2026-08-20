1470507d472a8c149d40afc32463e8a4c2e1ad61
Milind Kapadnis <milindkapadnis020@gmail.com>
2026-08-20 04:11:34 +0000

    Add application tests and Prometheus alerts

--- tests/test_main.py
+++ tests/test_main.py
@@ -3,9 +3,20 @@ from app.main import app
 
 client = TestClient(app)
 
+
 def test_home():
     response = client.get("/")
+
     assert response.status_code == 200
     assert response.json() == {
         "message": "Cloud Native DevOps Platform is running"
+    }
+
+
+def test_health():
+    response = client.get("/health")
+
+    assert response.status_code == 200
+    assert response.json() == {
+        "status": "healthy"
     }
