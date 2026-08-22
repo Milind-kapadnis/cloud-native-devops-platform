712ea4615f4f141359fd436c44ec540e89dd1cbb
Milind Kapadnis <milindkapadnis020@gmail.com>
2026-08-22 05:01:14 +0000

    feat: add Terraform infrastructure configuration

---
+++ terraform/main.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_version = ">= 1.0"
+
+  required_providers {
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.0"
+    }
+  }
+}
+
+provider "kubernetes" {
+  config_path = "~/.kube/config"
+}
+
+resource "kubernetes_namespace" "devops" {
+  metadata {
+    name = "devops"
+  }
+}
